a037c8b5485adf39e59477d3ae79a6e3cbd62a8a
Michael Jacobsen <mjacobsen@denverpost.com>
2016-02-24 11:56:28 -0700

    Users get_items()

--- lib/inc/class-wp-rest-coauthors-authorusers.php
+++ lib/inc/class-wp-rest-coauthors-authorusers.php
@@ -10,6 +10,20 @@
 
 class WP_REST_CoAuthors_AuthorUsers extends WP_REST_Controller {
 	/**
+	 * Post_type for Co-Authors.
+	 *
+	 * @var string
+	 */
+	protected $CoAuthors_Plus;
+
+	/**
+	 * Post_type for Co-Authors.
+	 *
+	 * @var string
+	 */
+	protected $CoAuthors_Guest_Authors;
+
+	/**
 	 * Taxonomy for Co-Authors.
 	 *
 	 * @var string
@@ -59,6 +73,8 @@ class WP_REST_CoAuthors_AuthorUsers extends WP_REST_Controller {
 		$this->parent_type = $parent_type;
 		$this->taxonomy = $taxonomy;
 		$this->post_type = $post_type;
+		$this->CoAuthors_Plus = new coauthors_plus ();
+		$this->CoAuthors_Guest_Authors = new CoAuthors_Guest_Authors();
 	}
 
 
@@ -69,45 +85,47 @@ class WP_REST_CoAuthors_AuthorUsers extends WP_REST_Controller {
 	 * @return WP_REST_Request|WP_Error, List of co-author objects data on success, WP_Error otherwise
 	 */
 	public function get_items( $request ) {
+
+		$authors = array();
 		if ( ! empty ( $request['parent_id'] ) ) {
 
 			$parent_id = (int) $request['parent_id'];
 
 			//Get the 'author' terms for this post
-			$terms = wp_get_object_terms( $parent_id, $this->taxonomy );
+			$author_terms = wp_get_object_terms( $parent_id, $this->taxonomy );
 		} else {
 			//Get all 'author' terms
-			$terms = get_terms( $this->taxonomy );
+			$author_terms = get_terms( $this->taxonomy );
 		}
 
-		foreach ( $terms as $term ) {
-			//create a map to look up the metadata in the term->description
-			//$searchmap = $this->set_searchmap($term); //Fail: see function
+		foreach ($author_terms as $author_term) {
 
-			//Since the co-authors method didn't work, trying regex for the int value of the ID
-			$regex = "/\\b(\\d+)\\b/";
-			preg_match( $regex, $term->description, $matches );
-			$id = $matches[1];
+			$coauthor = $this->CoAuthors_Plus->get_coauthor_by('user_login', $author_term->name, true);
 
-			// Get the user for this 'author' term
-			$user = get_user_by( 'id', $id );
+			if ( ! $coauthor ) {
+				continue;
+			}
 
-			if ( 'WP_User' == get_Class( $user ) ) {
-				// Enhance the object attributes for JSON
-				$author_user = $this->prepare_item_for_response( $user, $request );
+			$authors[] = $coauthor;
+		}
 
-				if ( is_wp_error( $author_user ) ) {
-					continue;
-				}
 
-				$author_users[] = $this->prepare_response_for_collection( $author_user );
+		foreach ( $authors as $author ) {
+
+			$author_post_item = $this->prepare_item_for_response( $author, $request );
+
+			if ( is_wp_error( $author_post_item ) ) {
+				continue;
+			}
+
+			if ( ! empty( $author_post_item ) ) {
+				$author_posts[] = $this->prepare_response_for_collection( $author_post_item );
 			}
 		}
 
-		if ( ! empty( $author_users ) ) {
-			return rest_ensure_response( $author_users );
+		if ( ! empty( $author_posts ) ) {
+			return rest_ensure_response( $author_posts );
 		}
-
 		return new WP_Error( 'rest_co_authors_get_users', __( 'Invalid authors id.' ), array( 'status' => 404 ) );
 	}
 
@@ -178,21 +196,26 @@ class WP_REST_CoAuthors_AuthorUsers extends WP_REST_Controller {
 	 * Prepares co-authors data for return as an object.
 	 * Used to prepare the guest-authors object
 	 *
-	 * @param WP_User $data
+	 * @param stdClass|WP_Post $data
 	 * @param WP_REST_Request $request
 	 * @return WP_REST_Response|WP_Error, co-authors object data on success, WP_Error otherwise
 	 */
 	public function prepare_item_for_response( $data, $request ) {
-		$author_user = array();
+		$author_user = array(
+			'id'                => (int) $data->ID,
+			'display_name'      => (string) $data->display_name,
+			'first_name'        => (string) $data->first_name,
+			'last_name'         => (string) $data->last_name,
+			'user_login'        => (string) $data->user_login,
+			'user_email'        => (string) $data->user_email,
+			'linked_account'    => (string) $data->linked_account,
+			'website'           => (string) $data->website,
+			'aim'               => (string) $data->aim,
+			'yahooim'           => (string) $data->yahooim,
+			'jabber'            => (string) $data->jabber,
+			'description'       => (string) $data->description,
+		);
 
-		if ( 'WP_User' == get_Class( $data )  ) {
-			$author_user = array(
-				'id'            => (int) $data->ID,
-				'first_name'    => (string) $data->first_name,
-				'last_name'     => (string) $data->last_name,
-				'display_name'  => (string) $data->display_name,
-			);
-		}
 
 		$response = rest_ensure_response( $author_user );
 
